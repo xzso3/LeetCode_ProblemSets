2e64ee5438d23511a82bc4ea742b2a903e07f621
Const <c@6w6.io>
2023-07-30 11:02:01 +0800

    Update Leetcode 141 & 142 solution

---
+++ LeetCode.ProblemSets.Solutions/LeetCode_141.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LeetCode.Problem141
+{
+    public class LeetCode_141
+    {
+        public bool HasCycle(ListNode head)
+        {
+            var fast = head;
+            var slow = head;
+            
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if (fast == slow)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+    
+}
+
---
+++ LeetCode.ProblemSets.Solutions/LeetCode_142.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.Problem141;
+
+public class LeetCode_142
+{
+    // Fast-Slow Pointer with counter.
+    public ListNode DetectCycle(ListNode head)
+    {
+        if (head == null || head.next == null)
+        {
+            return null;
+        }
+        
+        var fast = head;
+        var slow = head;
+        var loopFlag = false;
+
+        while (fast.next != null && fast.next.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow)
+            {
+                loopFlag = true;
+                break;
+            }
+        }
+
+        if (!loopFlag)
+        {
+            return null;
+        }
+
+        var roundCheck = head;
+    
+        while (roundCheck != slow)
+        {
+            roundCheck = roundCheck.next;
+            slow = slow.next;
+        }
+
+        return roundCheck;
+
+    }
+}
