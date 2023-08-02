2d47ddd33f61ff487b56facbf6f7a6e8ac2bc367
Const <c@6w6.io>
2023-08-02 21:44:11 +0800

    Finish LeetCode 21

---
+++ LeetCode.ProblemSets.Solutions/LeetCode_21.cs
@@ -0,0 +1,68 @@
+﻿public class LeetCode_21
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        if (list1 == null)
+        {
+            return list2;
+        }
+        
+        if (list2 == null)
+        {
+            return list1;
+        }
+
+        ListNode previous = null, head = list1;
+
+        while (list1.next != null && list2.next != null)
+        {
+            if (list1.val < list2.val)
+            {
+                previous = list1;
+                if (list1.next != null)
+                {
+                    list1 = list1.next;
+                }
+                else
+                {
+                    list1.next = list2;
+                    break;
+                }
+                
+            }
+            else
+            {
+                var tempList2Next = list2.next;
+                if (previous == null)
+                {
+                    head = list2;
+                    previous = head;
+
+                }
+                else
+                {
+                    previous.next = list2;
+                    previous = list2;
+                }
+                previous.next = list1;
+                list2 = tempList2Next;
+            }
+        }
+
+
+        return head;
+
+    }
+    
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
