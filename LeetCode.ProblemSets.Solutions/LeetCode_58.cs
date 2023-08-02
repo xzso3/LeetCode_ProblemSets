1613d2ec9b948fc0941ebf4d061e46b5fb4560a2
Const <c@6w6.io>
2023-08-02 21:44:33 +0800

    Finish LeetCode 58

---
+++ LeetCode.ProblemSets.Solutions/LeetCode_58.cs
@@ -0,0 +1,21 @@
+﻿public class LeetCode_58
+{
+    public int LengthOfLastWord(string s)
+    {
+        var length = 0;
+
+        for (var i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] == ' ')
+            {
+                if (length > 0) break;
+            }
+            else
+            {
+                length++;
+            }
+        }
+        
+        return length;
+    }
+}
