c14c1fad1b5098085effdfd83cdb18807011bccb
Const <c@6w6.io>
2023-08-02 21:44:19 +0800

    Finish LeetCode 27

---
+++ LeetCode.ProblemSets.Solutions/LeetCode_27.cs
@@ -0,0 +1,17 @@
+﻿public class LeetCode_27
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        var length = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[length++] = nums[i];
+            }
+        }
+
+        return length;
+    }
+}
