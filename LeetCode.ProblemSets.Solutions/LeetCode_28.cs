bff4babd4b2d5971e60dfa1016b7d7f99352c0eb
Const <c@6w6.io>
2023-08-02 21:44:23 +0800

    Finish LeetCode 28

---
+++ LeetCode.ProblemSets.Solutions/LeetCode_28.cs
@@ -0,0 +1,32 @@
+﻿public class LeetCode_28
+{
+    public int StrStr(string haystack, string needle)
+    {
+        var result = -1;
+        var compares = haystack.Length - needle.Length + 1;
+        for (var i = 0; i < compares; i++)
+        {
+            var flag = true;
+            for (var j = 0; j < needle.Length; j++)
+            {
+                if (haystack[i + j] != needle[j])
+                {
+                    flag = false;
+                    break;
+                }
+                else
+                {
+                    flag = true;
+                }
+            }
+
+            if (flag)
+            {
+                result = i;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
