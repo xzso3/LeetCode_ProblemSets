8fe46d07cac34258f3e317033101ec41c371f7e2
Const <c@6w6.io>
2023-08-02 21:44:15 +0800

    Finish LeetCode 26

---
+++ LeetCode.ProblemSets.Solutions/LeetCode_26.cs
@@ -0,0 +1,19 @@
+﻿public class LeetCode_26
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        var length = 1;
+        var prev = nums[0];
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != prev)
+            {
+                prev = nums[i];
+                nums[length++] = nums[i];
+            }
+        }
+
+        return length;
+    }
+}
