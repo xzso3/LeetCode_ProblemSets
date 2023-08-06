f78ed16882080ff17c95ba8462f02c92e3be8e4f
Const <c@6w6.io>
2023-08-06 10:49:22 +0800

    Finish LeetCode 24

---
+++ LeetCode.ProblemSets.Solutions/LeetCode_24.cs
@@ -0,0 +1,23 @@
+﻿public class LeetCode_24
+{
+    public ListNode SwapPairs(ListNode head)
+    {
+        if (head == null || head.next == null) return head;
+        var front = head;
+        var back = head.next;
+        var next = back.next;
+        back.next = front;
+        front.next = SwapPairs(next);
+        return back;
+    }
+    
+     public class ListNode {
+         public int val;
+         public ListNode next;
+         public ListNode(int val = 0, ListNode next = null) 
+         {
+            this.val = val;
+            this.next = next; 
+         }
+     }
+}
