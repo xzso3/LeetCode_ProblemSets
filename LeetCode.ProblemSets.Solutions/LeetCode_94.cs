7cd8cb3552db7a161125d0a9232a5f9d02ef4518
Const <c@6w6.io>
2023-08-07 14:55:13 +0800

    Finish LeetCode 94

---
+++ LeetCode.ProblemSets.Solutions/LeetCode_94.cs
@@ -0,0 +1,41 @@
+﻿public class LeetCode_94
+{
+    public List<int> result = new List<int>();
+
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        if (root == null) return new int[0];
+        Traversal(root);
+        return result;
+    }
+
+    public void Traversal(TreeNode node)
+    {
+        // SEARCH LEFT NODE
+        if (node.left != null)
+        {
+            Traversal(node.left);
+        }
+        // ADD SELF
+        result.Add(node.val);
+        // SEARCH RIGHT NODE
+        if (node.right != null)
+        {
+            Traversal(node.right);
+        }
+    }
+    
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
