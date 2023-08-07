96126d2431684af1a858165f5c61a7fc6df7ad29
Const <c@6w6.io>
2023-08-07 18:05:22 +0800

    Finish LeetCode 100,101,104

---
+++ LeetCode.ProblemSets.Solutions/LeetCode_101.cs
@@ -0,0 +1,30 @@
+﻿public class LeetCode_101
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        return CheckSymmetric(root.left, root.right);
+    }
+
+    public bool CheckSymmetric(TreeNode left, TreeNode right)
+    {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+
+        return CheckSymmetric(left.left, right.right) && CheckSymmetric(left.right, right.left);
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ LeetCode.ProblemSets.Solutions/LeetCode_104.cs
@@ -0,0 +1,38 @@
+﻿public class LeetCode_104
+{
+    public int MaxDepth(TreeNode root)
+    {
+        var depth = 0;
+
+        if (root == null)
+        {
+            return depth;
+        }
+        
+        depth++;
+        
+        if (root.left == null && root.right == null)
+        {
+            return depth;
+        }
+
+        var leftDepth = MaxDepth(root.left);
+        var rightDepth = MaxDepth(root.right);
+        return depth + (leftDepth > rightDepth ? leftDepth : rightDepth);
+
+    }
+    
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
